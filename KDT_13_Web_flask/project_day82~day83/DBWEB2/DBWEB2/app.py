9977d4f710821daeb1509c09a73323bf135eded4
songeun02 <se021216@yu.ac.kr>
2025-07-16 18:25:21 +0900

    Update app.py

--- KDT_13_Web_flask/project_day82~day83/DBWEB2/DBWEB2/app.py
+++ KDT_13_Web_flask/project_day82~day83/DBWEB2/DBWEB2/app.py
@@ -9,16 +9,16 @@ import torch.nn as nn
 app = Flask(__name__)
 
 # 모델 인스턴스 생성 및 가중치 로드
-model1=torch.load(r'C:\dlrj_rlaudwn\kdt\KDT-2\DBWEB2\models\top_model_all.pth', map_location='cpu')
+model1=torch.load(r'C:\Users\sh321\Desktop\KDT_KNU\KDT_13_Web_flask\project_day82~day83\DBWEB2\DBWEB2\models\top_model_all.pth', map_location='cpu',weights_only=False )
 model1.eval()
 
-model2=torch.load(r'C:\dlrj_rlaudwn\kdt\KDT-2\DBWEB2\models\bottom_model_all.pth', map_location='cpu')
+model2=torch.load(r'C:\Users\sh321\Desktop\KDT_KNU\KDT_13_Web_flask\project_day82~day83\DBWEB2\DBWEB2\models\bottom_model_all.pth', map_location='cpu',weights_only=False)
 model2.eval()
 
-model3=torch.load(r'C:\dlrj_rlaudwn\kdt\KDT-2\DBWEB2\models\shoes_model_all.pth', map_location='cpu')
+model3=torch.load(r'C:\Users\sh321\Desktop\KDT_KNU\KDT_13_Web_flask\project_day82~day83\DBWEB2\DBWEB2\models\shoes_model_all.pth', map_location='cpu',weights_only=False)
 model3.eval()
 
-model4=torch.load(r'C:\dlrj_rlaudwn\kdt\KDT-2\DBWEB2\models\acc_model_all.pth', map_location='cpu')
+model4=torch.load(r'C:\Users\sh321\Desktop\KDT_KNU\KDT_13_Web_flask\project_day82~day83\DBWEB2\DBWEB2\models\acc_model_all.pth', map_location='cpu',weights_only=False)
 model4.eval()
 
 UPLOAD_FOLDER = 'static/uploads/'
@@ -42,16 +42,21 @@ def index():
             transform = transforms.Compose([
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406],
+                                     [0.229, 0.224, 0.225])
             ])
             input_tensor = transform(image).unsqueeze(0)
 
             with torch.no_grad():
-                result1 = model1(input_tensor)
+                prob1 = torch.sigmoid(model1(input_tensor))[0].item()
+                prob2 = torch.sigmoid(model2(input_tensor))[0].item()
+                prob3 = torch.sigmoid(model3(input_tensor))[0].item()
+                prob4 = torch.sigmoid(model4(input_tensor))[0].item()
 
-            probabilities = nn.Softmax(dim=1)(result1)
-            predicted_label_index = probabilities.argmax().item()
-            class_labels = ["상의", "하의", "신발", "액세서리"]
-            predicted_label = class_labels[predicted_label_index]
+                probs = [prob1, prob2, prob3, prob4]
+                print(probs)
+                class_labels = ["상의", "하의", "신발", "액세서리"]
+                predicted_label = class_labels[probs.index(max(probs))]
 
             return render_template('index.html', title='패션 제품 분류하기', result=predicted_label)
 
@@ -95,34 +100,31 @@ def personal_page(username):
                 class_labels = ["액세서리", "하의", "신발", "상의"]
 
 
-            
-
             if model:
                 with torch.no_grad():
                     image = Image.open(filepath)
                     transform = transforms.Compose([
                         transforms.Resize((224, 224)),
                         transforms.ToTensor(),
+                        transforms.Normalize([0.485, 0.456, 0.406],
+                                             [0.229, 0.224, 0.225])
                     ])
                     input_tensor = transform(image).unsqueeze(0)
 
-                    result = model(input_tensor)  # 해당 사용자 모델 사용
-                    probabilities = nn.Softmax(dim=1)(result)
+                    probabilities = torch.sigmoid(model(input_tensor))[0].item()
                     print(probabilities)
-                    predicted_label_index = probabilities.argmax().item()
-                    predicted_label = class_labels[predicted_label_index]
 
+                threshold = 0.7
                 
                 # 결과 값을 사용자에 맞게 수정
                 if username == '상의 분류':
-                    result_text = f'{predicted_label} O' if predicted_label == "상의" else f' {predicted_label} X'
+                    result_text = '상의 O' if probabilities >= threshold else '상의 X'
                 elif username == '하의 분류':
-                    result_text = f'{predicted_label} O' if predicted_label == "하의" else f' {predicted_label} X'
+                    result_text = '하의 O' if probabilities >= threshold else '하의 X'
                 elif username == '신발 분류':
-                    result_text = f'{predicted_label} O' if predicted_label == "신발" else f' {predicted_label} X'
+                    result_text = '신발 O' if probabilities >= threshold else '신발 X'
                 elif username == '액세서리 분류':
-                    result_text = f'{predicted_label} O' if predicted_label == "액세서리" else f' {predicted_label} X'
-
+                    result_text = '액세서리 O' if probabilities >= threshold else '액세서리 X'
 
                 return render_template('index_personal.html', result=result_text, title=username)
             else:
